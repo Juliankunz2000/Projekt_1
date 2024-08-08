9338bee98350cc164e45049682c0b321807e7668
Juliankunz2000 <julian.kunz18@googlemail.com>
2024-08-08 13:10:49 +0200

    Fragen & Antworten hinzugefügt

---
+++ main.py
@@ -0,0 +1,17 @@
+questions_1 = ["Seine Mutter, die umarmt er - Seine Partnerin, die... ", 'Wen gewisse Umstände misstrauisch gemacht haben, der kommt zu der Vermutung „Hier ist etwas ...“?',
+                "Was machen nicht nur Musiker, wenn sie jemanden scharf zurechtweisen?",
+                "Geht die Amphibie reichlich resolut vor, dann ist ...?", 'Ein „Longsleeve“ kennt der Online-Duden als die langärmelige Version vom ...?',
+                "Laut Statistischem Bundesamt wurden 2016 und 2020 an welchem Datum auffällig weniger Kinder geboren als im Schnitt?",
+                "Bezogen auf die Mengenanteile der drei Hauptzutaten nennt man ...?", "Wer taucht seit dem 14. Januar 2024 vermehrt in den Schlagzeilen der internationalen Regenbogenpresse auf?",
+                "In welcher Sportart müssen die Athleten die Wurfuhr im Blick haben?", "Mit wem hatten Paul Janke, Detlef Soost, Ingolf Lück, Pascal Hens und Bastian Bielendorfer nachweislich engen Körperkontakt?",
+                'Ein eisernes Element mit sechs „Zähnen“, die für die sechs Stadtteile stehen sollen, befindet sich am ...?', "Einen praktischen Frunk findet man in vielen ...?",
+                'In wessen Metier fällt die Fertigkeit, etwas fachgerecht „aus der Decke zu schlagen“?', "Was ist zuweilen mit einem Schlupfriemen ausgestattet?",
+               'Wer bot einst über sein Weingut die Tropfen „Klöbener Krötenpfuhl“, „Oberföhringer Vogelspinne“ und „Hupfheimer Jungferngärtchen“ an?']
+wrong_answers_1 = [["pfar er", "kardinn al", "passt or"], ["müde", "bequem", "träge"],
+                    ["Standpunkt flöten", "Ansicht trommeln", "Auffassung orgeln"], ["die Kröte körte", "die Unke ukne", "der Salamander samalander"],
+                    ["Cocktailkleid", "Hawaiihemd", "Büstenhalter"], ["29. April", "29. Juni", "29. August"], ["Rührei auch 1-2-3-Ei", "Grießbrei auch 1-2-3-Brei", "Glühwein auch 1-2-3-Wein"],
+                    ["Andrea Doria", "Gorch Fock", "Santa Maria"], ["Handball", "Völkerball", "Fußball"], ["Evelyn Burdecki", "Laura Wontorra", "Verona Pooth"], ["Kühlergrill Londoner Taxis", "Kutschblock Wiener Fiaker", "Eingang zur Pariser Métro"],
+                    ["E-Zahnbürsten", "E-Zigaretten", "E-Mails"], ["Konditor", "Bergmann", "Golfer"],
+                    ["Automotoren", "Ruderboote", "Armbanduhren"], ["CDU", "DFB", "ADAC"]]
+right_answers_1 = ["küsst er", "faul", "Meinung geigen", "der Frosch forsch", "T-Shirt", "29. Februar", "Mürbeteig auch 1-2-3-Teig", "Queen Mary", "Basketball",
+                    "Ekaterina Leonova", "Bug venezianischer Gondeln", "E-Autos", "Weidmann", "Lederstiefel", "DRK"]
