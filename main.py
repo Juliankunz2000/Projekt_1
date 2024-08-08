22aa96684d73d6fe2daef7f347d08470301b4972
Juliankunz2000 <julian.kunz18@googlemail.com>
2024-08-08 14:09:55 +0200

    Funktionalität hinzugefügt

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 questions_1 = ["Seine Mutter, die umarmt er - Seine Partnerin, die... ", 'Wen gewisse Umstände misstrauisch gemacht haben, der kommt zu der Vermutung „Hier ist etwas ...“?',
                 "Was machen nicht nur Musiker, wenn sie jemanden scharf zurechtweisen?",
                 "Geht die Amphibie reichlich resolut vor, dann ist ...?", 'Ein „Longsleeve“ kennt der Online-Duden als die langärmelige Version vom ...?',
@@ -15,3 +17,71 @@ wrong_answers_1 = [["pfar er", "kardinn al", "passt or"], ["müde", "bequem", "t
                     ["Automotoren", "Ruderboote", "Armbanduhren"], ["CDU", "DFB", "ADAC"]]
 right_answers_1 = ["küsst er", "faul", "Meinung geigen", "der Frosch forsch", "T-Shirt", "29. Februar", "Mürbeteig auch 1-2-3-Teig", "Queen Mary", "Basketball",
                     "Ekaterina Leonova", "Bug venezianischer Gondeln", "E-Autos", "Weidmann", "Lederstiefel", "DRK"]
+
+
+Kontostand = 0
+Joker = 2
+round = 1
+
+
+# Ab hier
+
+possible_choices = [wrong_answers_1[round-1][0], right_answers_1[round-1], wrong_answers_1[round-1][1],
+                     wrong_answers_1[round-1][2]]
+
+
+choice1 = random.choice(possible_choices)
+index = possible_choices.index(choice1)
+possible_choices.pop(index)
+choice2 = random.choice(possible_choices)
+index = possible_choices.index(choice2)
+possible_choices.pop(index)
+choice3 = random.choice(possible_choices)
+index = possible_choices.index(choice3)
+possible_choices.pop(index)
+choice4 = random.choice(possible_choices)
+index = possible_choices.index(choice4)
+possible_choices.pop(index)
+
+print(f"Frage {round}: " + questions_1[round-1])
+print(f"A: {choice1}")
+print(f"B: {choice2}")
+print(f"C: {choice3}")
+print(f"D: {choice4}")
+print('Für den 50:50 Joker, gebe "x" ein!')
+
+antwort = input("Gib hier deine Antwort ein: ")
+
+if antwort == "A" and (choice1 == right_answers_1[round - 1]):
+    print("Herzlichen Glückwunsch, die Antwort ist richtig!")
+elif antwort == "B" and (choice2 == right_answers_1[round-1]):
+        print("Herzlichen Glückwunsch, die Antwort ist richtig!")
+elif antwort == "C" and (choice3 == right_answers_1[round-1]):
+        print("Herzlichen Glückwunsch, die Antwort ist richtig!")
+elif antwort == "D" and (choice4 == right_answers_1[round-1]):
+        print("Herzlichen Glückwunsch, die Antwort ist richtig!")
+elif antwort == "x":
+    possible_choices = [wrong_answers_1[round-1][0], right_answers_1[round-1]]
+    choice1 = random.choice(possible_choices)
+    index = possible_choices.index(choice1)
+    possible_choices.pop(index)
+    choice2 = random.choice(possible_choices)
+    index = possible_choices.index(choice2)
+    possible_choices.pop(index)
+    print(f"Frage {round}: " + questions_1[round - 1])
+    print(f"A: {choice1}")
+    print(f"B: {choice2}")
+    antwort = input("Gib hier deine Antwort ein: ")
+    if antwort == "A" and (choice1 == right_answers_1[round-1]):
+        print("Herzlichen Glückwunsch, die Antwort ist richtig!")
+    elif antwort == "B" and (choice2 == right_answers_1[round-1]):
+        print("Herzlichen Glückwunsch, die Antwort ist richtig!")
+    else:
+        print("Die Antwort ist leider falsch!")
+else:
+    print("Die Antwort ist leider falsch!")
+
+
+
+
+
